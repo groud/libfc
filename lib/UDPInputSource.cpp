e371811434b58c5f9f52faa94e2d7561a2fb2c12
Gilles Roudiere <gilles.roudiere@laas.fr>
2017-08-25 15:51:30 +0200

    Implement UDP source

--- lib/UDPInputSource.cpp
+++ lib/UDPInputSource.cpp
@@ -1,25 +1,25 @@
 /* Copyright (c) 2011-2014 ETH Zürich. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *    * Redistributions of source code must retain the above copyright
  *      notice, this list of conditions and the following disclaimer.
  *    * Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *    * Neither the names of ETH Zürich nor the names of other contributors 
- *      may be used to endorse or promote products derived from this software 
+ *    * Neither the names of ETH Zürich nor the names of other contributors
+ *      may be used to endorse or promote products derived from this software
  *      without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ETH 
- * ZURICH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ETH
+ * ZURICH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
@@ -28,38 +28,48 @@
 #include <sys/socket.h>
 
 #include "UDPInputSource.h"
-
+#include <stdio.h>
 namespace libfc {
 
-  UDPInputSource::UDPInputSource(const struct sockaddr* _sa, size_t _sa_len,
-                                 int _fd) 
-    : sa_len(_sa_len), fd(_fd) {
-    memcpy(&sa, _sa, _sa_len);
+UDPInputSource::UDPInputSource(const struct sockaddr *_remote,
+                               size_t _remote_len, int _fd)
+    : remote_len(_remote_len), fd(_fd) {
+  memcpy(&remote, _remote, _remote_len);
+}
+
+ssize_t UDPInputSource::read(uint8_t *buf, uint16_t len) {
+  struct sockaddr received_sa;
+  socklen_t received_sa_len;
+
+  // Wait for a packet if needed
+  if (packet_read == packet_lenght) {
+    packet_lenght = recvfrom(fd, packet_buffer, sizeof(packet_buffer), 0,
+                             &received_sa, &received_sa_len);
+    packet_read = 0;
   }
 
-  ssize_t UDPInputSource::read(uint8_t* buf, uint16_t len) {
-    // TODO
-    return 0;
+  if (packet_read + len <= packet_lenght) {
+    memcpy(buf, packet_buffer + packet_read, len);
+    packet_read += len;
+  } else {
+    return -1;
   }
 
-  bool UDPInputSource::resync() {
-    return true;
-  }
+  return len;
+}
 
-  size_t UDPInputSource::get_message_offset() const {
-    return 0;
-  }
+bool UDPInputSource::resync() {
+  packet_read = 0;
+  packet_lenght = 0;
+  return true;
+}
 
-  void UDPInputSource::advance_message_offset() {
-  }
+size_t UDPInputSource::get_message_offset() const { return 0; }
 
-  const char* UDPInputSource::get_name() const {
-    return "<UDP socket>";
-  }
+void UDPInputSource::advance_message_offset() {}
 
-  bool UDPInputSource::can_peek() const {
-    return false;
-  }
+const char *UDPInputSource::get_name() const { return "<UDP socket>"; }
 
+bool UDPInputSource::can_peek() const { return false; }
 
 } // namespace libfc
--- lib/UDPInputSource.h
+++ lib/UDPInputSource.h
@@ -1,26 +1,26 @@
 /* Hi Emacs, please use -*- mode: C++; -*- */
 /* Copyright (c) 2011-2014 ETH Zürich. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *    * Redistributions of source code must retain the above copyright
  *      notice, this list of conditions and the following disclaimer.
  *    * Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *    * Neither the names of ETH Zürich nor the names of other contributors 
- *      may be used to endorse or promote products derived from this software 
+ *    * Neither the names of ETH Zürich nor the names of other contributors
+ *      may be used to endorse or promote products derived from this software
  *      without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ETH 
- * ZURICH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY 
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ETH
+ * ZURICH BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
@@ -31,34 +31,38 @@
  */
 
 #ifndef _LIBFC_UDPINPUTSOURCE_H_
-#  define _LIBFC_UDPINPUTSOURCE_H_
+#define _LIBFC_UDPINPUTSOURCE_H_
 
-#  include "InputSource.h"
+#include "InputSource.h"
 
 namespace libfc {
 
-  class UDPInputSource : public InputSource {
-  public:
-    /** Creates a UDP input source from a file descriptor.
-     *
-     * @param sa the socket address of the peer from whom we accept messages
-     * @param sa_len the length of the socket address, in bytes
-     * @param fd the file descriptor belonging to a UDP socket
-     */
-    UDPInputSource(const struct sockaddr* sa, size_t sa_len, int fd);
+class UDPInputSource : public InputSource {
+public:
+  /** Creates a UDP input source from a file descriptor.
+   *
+   * @param remote the socket address of the peer from whom we accept messages
+   * @param remote_len the length of the socket address, in bytes
+   * @param fd the file descriptor belonging to a UDP socket
+   */
+  UDPInputSource(const struct sockaddr *remote, size_t remote_len, int fd);
 
-    ssize_t read(uint8_t* buf, uint16_t len);
-    bool resync();
-    size_t get_message_offset() const;
-    void advance_message_offset();
-    const char* get_name() const;
-    bool can_peek() const;
+  ssize_t read(uint8_t *buf, uint16_t len);
+  bool resync();
+  size_t get_message_offset() const;
+  void advance_message_offset();
+  const char *get_name() const;
+  bool can_peek() const;
 
-  private:
-    struct sockaddr sa;
-    size_t sa_len;
-    int fd;
-  };
+private:
+  uint8_t packet_buffer[4096];
+  ssize_t packet_lenght = 0;
+  ssize_t packet_read = 0;
+
+  struct sockaddr remote;
+  size_t remote_len;
+  int fd;
+};
 
 } // namespace libfc
 
